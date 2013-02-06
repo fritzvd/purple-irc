9732415eb58149f3ea2d28eb9ec4d13a89a55a45
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2013-02-06 20:02:46 +0100

    removed db stuff to init..

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import sqlite3
 from flask import *
-from contextlib import closing
 app = Flask(__name__)
 app.config.from_object(__name__)
 
@@ -11,15 +10,13 @@ SECRET_KEY = 'dev_key'
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
-def init_db():
-	with closing(connect_db()) as db:
-		with app.open_resource('schema.sql') as f:
-			db.cursor().executescript(f.read())
-		db.commit
-
 @app.before_request
 def before_request():
 	g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()
 
 if __name__== "__main__":
 	app.run()
